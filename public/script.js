f94adf2d7b26867073b4e1f03b535af2dafb0431
Glitch (hello-express) <none>
2020-05-28 16:31:15 +0000

    👷🌯 Checkpoint ./public/script.js:1359010/890

--- public/script.js
+++ public/script.js
@@ -2,46 +2,39 @@
 // run by the browser each time the page is loaded
 
 // define variables that reference elements on our page
-const eintrageListe = document.getElementById("eintraege");
+const eintraegeListe = document.getElementById("eintraege");
 const eintraegeForm = document.querySelector("form");
 
 
 function neuenEintragHinzufuegen(eintrag) {
   const newListItem = document.createElement("p");
   newListItem.innerText = eintrag;
-  eintrae
+  eintraegeListe.append(newListItem);
   
-}
-
-// a helper function that creates a list item for a given dream
-function appendNewDream(dream) {
-  const newListItem = document.createElement("li");
-  newListItem.innerText = dream;
-  dreamsList.appendChild(newListItem);
 }
 
 // fetch the initial list of dreams
 fetch("/eintraege")
   .then(response => response.json()) // parse the JSON from the server
-  .then(dreams => {
-    // remove the loading text
-    dreamsList.firstElementChild.remove();
+  .then(eintraege => {
+    // Platzhaltertext entfernen
+    eintraegeListe.firstElementChild.remove();
   
-    // iterate through every dream and add it to our page
-    dreams.forEach(appendNewDream);
+    // alle eintraege werden hinzugefügt
+    eintraege.forEach(neuenEintragHinzufuegen);
   
     // listen for the form to be submitted and add a new dream when it is
-    dreamsForm.addEventListener("submit", event => {
+    eintraegeForm.addEventListener("submit", event => {
       // stop our form submission from refreshing the page
       event.preventDefault();
 
       // get dream value and add it to the list
-      let newDream = dreamsForm.elements.dream.value;
-      dreams.push(newDream);
-      appendNewDream(newDream);
+      let neuerEintrag = eintraegeForm.elements.eintrag.value;
+      eintraege.push(neuerEintrag);
+      neuenEintragHinzufuegen(neuerEintrag);
 
       // reset form
-      dreamsForm.reset();
-      dreamsForm.elements.dream.focus();
+      eintraegeForm.reset();
+      eintraegeForm.elements.dream.focus();
     });
   });
