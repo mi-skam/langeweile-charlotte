d23431d02d01fa0f1309d6b59144f5d574355176
mi-skam <maksim.codes@mailbox.org>
2020-06-08 11:22:16 +0000

    port -> process.env.PORT

--- server.js
+++ server.js
@@ -86,6 +86,6 @@ const cleanseString = function (string) {
 };
 
 // listen for requests :)
-const listener = app.listen(46793, () => {
+const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
